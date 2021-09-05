f6a71cee11f88d15f15f51f39ecfdbfa491684ba
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2021-09-05 11:28:17 +0200

    FIX ENV

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: '3ddae554f17e11b3cf44a42c5f25910e',
-  url: 'https://api.themoviedb.org/3/',
-  imgPath: 'https://image.tmdb.org/t/p'
-};
