46eba337105f3f3e72f512d2c06fd76ee6657295
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2021-09-05 11:36:47 +0200

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiKey: 'Your_Api_Key',
+  apiKey: '3ddae554f17e11b3cf44a42c5f25910e',
   url: 'https://api.themoviedb.org/3/',
   imgPath: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: 'Your_Api_Key',
+  apiKey: '3ddae554f17e11b3cf44a42c5f25910e',
   url: 'https://api.themoviedb.org/3/',
   imgPath: 'https://image.tmdb.org/t/p'
 };
