340daadd94ebd72d29030adb0415062935cbf188
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2021-09-05 11:23:19 +0200

    ENV and gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: 'YOUR_API_KEY',
+  apiKey: '3ddae554f17e11b3cf44a42c5f25910e',
   url: 'https://api.themoviedb.org/3/',
   imgPath: 'https://image.tmdb.org/t/p'
 };
