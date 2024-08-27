83f96d6309b60597c6be9c3a3d4f979303612b04
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2024-08-27 16:28:13 +0200

    fix people api service

--- src/app/services/people-api.service.ts
+++ src/app/services/people-api.service.ts
@@ -5,18 +5,17 @@ import { environment } from 'src/environments/environment';
 import { PersonDetail } from '../interfaces/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PeopleApiService {
-
   private url = environment.url;
   private apiKey = environment.apiKey;
 
-  constructor(private http: HttpClient, private translate: TranslateService) { }
+  constructor(private http: HttpClient, private translate: TranslateService) {}
 
-  getActorDetail(actorId: number): Promise<PersonDetail>{
+  getActorDetail(actorId: number): Promise<PersonDetail> {
     const lang = this.translate.currentLang;
-    const url = `${this.url}/person/${actorId}?api_key=${this.apiKey}&language=${lang}&append_to_response=movie_credits,images`;
+    const url = `${this.url}person/${actorId}?api_key=${this.apiKey}&language=${lang}&append_to_response=movie_credits,images`;
     return this.http.get<PersonDetail>(url).toPromise();
   }
 }
